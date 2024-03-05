2a5f1caaa5ad0bc0248c793a2b7e0473b7347c33
Tadhg Ryan <tadhgryan2012@gmail.com>
2024-03-05 12:39:44 +0000

    [Tadhg] Added base navbar layout.

--- NavBar/index.php
+++ NavBar/index.php
@@ -1,13 +1,24 @@
-<nav class="navbar navbar-default">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<a class="navbar-brand" href="#">WebSiteName</a>
-		</div>
-		<ul class="nav navbar-nav">
-			<li class="active"><a href="#">Home</a></li>
-			<li><a href="#">Page 1</a></li>
-			<li><a href="#">Page 2</a></li>
-			<li><a href="#">Page 3</a></li>
-	  	</ul>
-	</div>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark navBarBackground">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../Home/index.html">
+            <img class="nav-brand logo" src="../resources/logo.png" alt="Logo">
+        </a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="navBarLinks bi bi-search"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-link navBarLinks bi bi-envelope"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-link navBarLinks bi bi-person-circle"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
 </nav>
