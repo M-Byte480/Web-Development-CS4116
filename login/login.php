8029fcd1764f99def58e47d545dd0eacf4077b35
Kevin <21037571@studentmail.ul.ie>
2024-04-04 00:53:47 +0100

    [Kevin] Cooking with user verification, needs testing

--- login/login.php
+++ login/login.php
@@ -6,25 +6,41 @@ require_once(__DIR__ . "/../database/repositories/users.php");
 
 print_r($_POST);
 
-if (isset($_POST['db_username']) && isset($_POST['db_password'])) {
+if (isset($_POST['email']) && isset($_POST['db_password'])) {
 
-    function validate($data)
-    {
-        $data = trim($data);
+    $email = $_POST['email'];
+    $password = $_POST['db_password'];
 
-        $data = stripslashes($data);
+    if (empty($email) || empty($password)) {
+        echo "Please fill in all text fields";
+    } else {
+        $dquery = "SELECT * FROM Users where email =?";
 
-        $data = htmlspecialchars($data);
+        $st = $conn->prepare($dquery);
+        $st->bind_param("s", $email);
+        $st->execute();
 
-        return $data;
+        $res = $st->get_result();
+
+        if ($res->num_rows > 0) {
+            $row = $res->fetch_assoc();
+
+            if (password_verify($password, $row['password'])) {
+
+                $_SESSION['user id'] = $row['id'];
+                echo "Login Successful";
+            } else {
+                echo "Invalid Login";
+            }
+        } else {
+            echo "Invalid Login";
+        }
     }
+} else {
+    echo "Database Query Error";
 }
 
-$e = validate_email($_POST['email']);
-
-$pass = validate($_POST['db_password']);
-
-if (empty($e)) {
-    header("location");
-}
 ?>
+
+
+
