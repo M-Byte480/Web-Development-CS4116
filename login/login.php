e5ee6eec09233b1143da69eccc6ca5c7bc42647d
Caoimhin <116285183+CaoimhinHawk1@users.noreply.github.com>
2024-04-04 17:13:33 +0100

    [Kevin] Worked on login backend, added info.php to check of the php file installed

--- login/login.php
+++ login/login.php
@@ -2,43 +2,69 @@
 
 session_start();
 
+global $db_host, $db_username, $db_password, $db_database;
+
 require_once(__DIR__ . "/../database/repositories/users.php");
+require_once(__DIR__ . '/../secrets.settings.php');
 
 print_r($_POST);
 
-if (isset($_POST['email']) && isset($_POST['db_password'])) {
-
-    $email = $_POST['email'];
-    $password = $_POST['db_password'];
-
-    if (empty($email) || empty($password)) {
-        echo "Please fill in all text fields";
-    } else {
-        $dquery = "SELECT * FROM Users where email =?";
-
-        $st = $conn->prepare($dquery);
-        $st->bind_param("s", $email);
-        $st->execute();
-
-        $res = $st->get_result();
-
-        if ($res->num_rows > 0) {
-            $row = $res->fetch_assoc();
-
-            if (password_verify($password, $row['password'])) {
-
-                $_SESSION['user id'] = $row['id'];
-                echo "Login Successful";
-            } else {
-                echo "Invalid Login";
-            }
-        } else {
-            echo "Invalid Login";
-        }
-    }
-} else {
-    echo "Database Query Error";
+if (!(isset($_POST['email']) && isset($_POST['password']))) {
+    echo "No email or password set";
+    exit();
 }
+
+$email = $_POST['email'];
+
+$password = $_POST['password'];
+
+if (empty($email) || empty($password)) {
+    echo "Please fill in all text fields";
+    var_dump(function_exists('mysqli_connect'));
+    exit();
+}
+
+$query = "SELECT * FROM users where email = ?";
+
+$con = mysqli_connect($db_host, $db_username, $db_password, $db_database);
+
+if ($con->connect_error) {
+    die("Connection Failed: " . $con->connect_error);
+}
+$st = $con->prepare($query);
+
+if ((!$st)) {
+    die("Error occurred in preparing statement: " . $con->error);
+}
+
+$st->bind_param("s", $email);
+$st->execute();
+
+$res = $st->get_result();
+
+if (!$res) {
+    die("Error occurred in getting result: " . $con->error);
+}
+
+if ($res->num_rows === 0) {
+    echo "Invalid Login";
+    exit();
+}
+
+$row = $res->fetch_assoc();
+
+if (!isset($row['password'])) {
+    die("Error: password is not contained in fetched row");
+}
+
+if (password_verify($password, $row['password'])) {
+
+    $_SESSION['user id'] = $row['id'];
+    echo "Login Successful";
+} else {
+    echo "Invalid Login";
+}
+
 
 ?>
 
