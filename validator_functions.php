24c35f822b9ed4f1b1697c0743d8d53f89ce9df9
Tadhg Ryan <tadhgryan2012@gmail.com>
2024-04-08 15:50:47 +0100

    [Tadhg] Updated nav bar to include admin page, logout and shows depending on logged in and admin.

--- validator_functions.php
+++ validator_functions.php
@@ -113,7 +113,9 @@ function validate_user_logged_in(): void
     validate_unique_result($query_result);
 }
 
-
+/**
+ * @throws ValidationException
+ */
 function validate_user_is_admin(): void
 {
     // Validation
@@ -130,8 +132,7 @@ function validate_user_is_admin(): void
 
     // Both these need to be true
     if ($user == null || !validate_admin($user['id'])) {
-        echo 'Unauthorised';
-        exit();
+        throw new ValidationException('Unauthorised');
     }
 
     // Free the buffer/memory
