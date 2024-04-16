969d13c8675833021941c90088ff26663268f9b9
Breny <21315205@studentmail.ul.ie>
2024-04-16 15:57:53 +0100

    [Breny] rebasing

--- validator_functions.php
+++ validator_functions.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/exceptions/ValidationException.php');
 function validate_email($email): false|int
 {
     // Regex
-    $regex = '/^(?!\.)[A-Za-z0-9.]+@[A-Za-z]+[A-Za-z0-9]*\.[A-Za-z]+$/';
+    $regex = '/^(?!\.)[A-Za-z0-9.]+@[A-Za-z]+[A-Za-z0-9.]*[A-Za-z]+$/';
     return preg_match($regex, $email);
 }
 
