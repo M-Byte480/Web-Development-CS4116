5f856143bd44a46fd165f49740835ccb88090dee
Breny <21315205@studentmail.ul.ie>
2024-04-17 02:07:52 +0100

    [Breny] Quick PR to fix the gender logging to the db

--- Signup/signup_backend.php
+++ Signup/signup_backend.php
@@ -9,10 +9,11 @@ require_once(__DIR__ . '/../database/repositories/users.php');
 $id = custom_uuid();
 $errors = validateData();
 $output = array();
+$gender = $_POST["gender"];
 
 if (empty($errors)) {
     set_id_email_pw_fname_lname_dob_jd($db_host, $db_username, $db_password, $db_database, $id);
-    set_id_gender($db_host, $db_username, $db_password, $db_database, $id);
+    set_id_gender($db_host, $db_username, $db_password, $db_database, $id, $gender);
     $output['success'] = 1;
 } else {
     $output['success'] = 0;
