f3e20d1cea2a997c384e2a2b97931be591fe91ea
Kevin <21037571@studentmail.ul.ie>
2024-04-17 01:54:54 +0100

    [Kevin] Creation of Messaging Index                      \`.__..--'' `.                      ( _          ,\                     ( <_< < <   `','`.                      \ (_< < <    \   `.                       `. `----'   (  q _p                         `-._  _.-' `-(_''\                          (_'))--,      `._\                             `-._<                                     hjw

--- messages/index.php
+++ messages/index.php
@@ -4,129 +4,68 @@
     <?php require_once("../imports.php"); ?>
     <title>Messaging</title>
 </head>
-<body class="bg-secondary" style="overflow-y: hidden;">
+<body>
 <?php require_once("../nav_bar/index.php") ?>
-
-<style>
-
-    .zoom {
-        transition: transform .4s;
-    }
-
-    .zoom:hover {
-        transform: scale(1.02);
-        box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
-    }
-
-    .form-control:focus {
-        outline: none !important;
-    }
-
-</style>
-<script>
-    const tabElms = document.querySelectorAll('a[data-bs-toggle="list"]')
-    tabElms.forEach(tabElm => {
-        tabElm.addEventListener('shown.bs.tab', event => {
-            event.target.setAttribute("active"); // newly activated tab
-            event.relatedTarget.removeAttribute("active"); // previous active tab
-        })
-    });
-</script>
-
-<div class="row m-0">
-    <div class="col-4 p-0 pe-1">
-        <!-- Message threads -->
-        <div class="list-group list-group-flush px-1 overflow-scroll" id="messageThreads" style="height: 0">
-            <?php
-            for ($i = 0; $i < 15; $i++) {
-                ?>
-                <a href="#"
-                   class="list-group-item list-group-item-action border-0 bg-secondary p-0 rounded-3 zoom"
-                   data-bs-toggle="list">
-                    <div class="p-2 m-0">
-                        <div class="row p-2 m-0">
-                            <div class="col-3 p-0">
-                                <img class="img-fluid rounded-3 mx-auto" src="../resources/milanPFP.png"
-                                     alt="Profile picture">
+<div class="container">
+    <div class="row">
+        <div class="col-4 col-12">
+            <div class="card messages p-3 m-0  bd-example m-0 ">
+                <div id="connections" class="connection-list">
+                    <ul class="list-unstyled list-group-flush chl mt-2 mb-0">
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <img src="../resources/pfps/Screenshot_2023-07-31_203438.png" alt="breny"
+                                 class="rounded-circle">
+                            <div class="info">
+                                <div class="name">Brendan Quinn</div>
+                                <div class="lmessage">" shup will ya "</div>
                             </div>
-                            <div class="col-9 p-0 ps-3 pt-1">
-                                <div>
-                                    <h5>Kevin</h5>
-                                    <i>Hello, are you still awake?</i>
-                                </div>
+                            <span class="badge text-bg-primary rounded-pill">12</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                            <img src="../resources/pfps/Screenshot_2023-07-31_203438.png" alt="breny"
+                                 class="rounded-circle">
+                            <div class="info">
+                                <div class="name">Brendan Quinn</div>
+                                <div class="lmessage">" shup will ya "</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="chat-box">
+                <div class="chat-head clearfix">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a>
+                                <img src="../resources/pfps/Screenshot_2023-07-31_203438.png" alt="breny"
+                                     class="rounded-circle">
+                            </a>
+                            <div class="cinfo">
+                                <h4 class="m-b-0">Breny</h4>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <?php
-            }
-            ?>
-        </div>
-    </div>
-    <div class="col-8 p-0 position-relative">
-        <div class="p-2 fs-2">
-            <div class="d-inline-flex">
-                <b>Kevin</b>
-            </div>
-            <div class="btn-group float-end">
-                <button class="bg-transparent border-0 p-0 float-end" type="button"
-                        id="userOptionsDropdown"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                    <a class="nav-link float-end">
-                        <i class="bi bi-three-dots-vertical text-white"></i>
-                    </a>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-md-end position-absolute"
-                    aria-labelledby="userOptionsDropdown">
-                    <li>
-                        <a class="float-end dropdown-item" href="#">
-                            <h4>Profile</h4>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="float-end dropdown-item" href="#">
-                            <h4>Report</h4>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="float-end dropdown-item" href="#">
-                            <h4>Delete Chat</h4>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="row m-0 bg-white rounded-start border border-2 border-end-0"
-             style="border-color:#ff8527!important;" id="messageWindow">
-            <!-- Messages -->
-        </div>
-        <div class="m-0 p-3 position-absolute bottom-0 mb-2 w-100" id="textInput">
-            <div class="form p-0">
-                <div class="row p-0">
-                    <div class="col-10 p-0">
-                        <input type="text" class="form-control bg-secondary border-0"
-                               placeholder="Start typing...">
-                    </div>
-                    <div class="col-2 p-0 ps-3">
-                        <button class="btn bg-white rounded-3" type="button"
-                                style="--bs-bg-opacity: .1;">
-                            Send
-                        </button>
+                        <div class="col-lg-6 text-right">
+                            <button class="btn" type="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                <svg width="12" height="14" fill="currentColor" class="bi bi-three-dots-vertical"
+                                     viewBox="0 0 16 16">
+                                    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                                </svg>
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-light bg-light">
+                                <li><a class="dropdown-item text-warning" href="#">Report</a></li>
+                                <li>
+                                    <hr class="dropdown-divider border-top border-secondary">
+                                </li>
+                                <li><a class="dropdown-item text-danger" href="#">Delete chat</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script>
-    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    let topOfMessageThread = document.getElementById("messageThreads").getBoundingClientRect().top;
-    let topOfMessageWindow = document.getElementById("messageWindow").getBoundingClientRect().top;
-    let heightOfTextInput = document.getElementById("textInput").getBoundingClientRect().height;
-
-    document.getElementById("messageThreads").style.height = (vh - topOfMessageThread).toString() + "px";
-    document.getElementById("messageWindow").style.height = (vh - topOfMessageWindow - heightOfTextInput).toString() + "px";
-</script>
 </body>
 </html>
 
