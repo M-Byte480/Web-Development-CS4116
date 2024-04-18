1de8fe29f6dc45f80dd41cdeb3d36e4dbecf6874
Milan <kmilanm13@gmail.com>
2024-04-19 00:56:39 +0100

    [Milan] Revised Signup

--- Signup/signup_backend.php
+++ Signup/signup_backend.php
@@ -9,11 +9,22 @@ require_once(__DIR__ . '/../database/repositories/users.php');
 $id = custom_uuid();
 $errors = validateData();
 $output = array();
+
 $gender = $_POST["gender"];
+$password = $_POST["user_password"];
+$email = $_POST["user_email"];
+$first_name = $_POST["user_first_name"];
+$second_name = $_POST["user_second_name"];
+$date_of_birth = date('Y-m-d', strtotime($_POST["user_dob"]));
+$email_in_use = is_email_taken($email);
+
+if($email_in_use){
+    $errors[] = "Email already linked to an existing account";
+}
 
 if (empty($errors)) {
-    set_id_email_pw_fname_lname_dob_jd($id);
-    set_id_gender($id, $gender);
+    add_user_to_database($id, $email, $first_name, $second_name, $password, $date_of_birth);
+    add_new_row_to_profile($id, $gender);
     $output['success'] = 1;
 } else {
     $output['success'] = 0;
