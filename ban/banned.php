d2c518e76797b23f543b81d0431f4b746521ecd7
Kevin <21037571@studentmail.ul.ie>
2024-04-21 15:27:18 +0100

    [Kevin] First edition of ban page

--- ban/banned.php
+++ ban/banned.php
@@ -1,13 +1,29 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <?php
+    require_once(__DIR__ . "/../imports.php")
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        
+    </style>
     <title>Document</title>
 </head>
 <body>
-    You are banned kid!
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3 col-xl-4 col-xs-12">
+            <img src="../resources/banned/bannedimage.jpg" alt="banned">
+        </div>
+        <div class="col-md-3 col-xl-4 col-xs-12">
+            <h1>You just got BANNNED Kid</h1>
+            <p>You better get out before I beat you into a pulp >:(</p>
+        </div>
+    </div>
+</div>
 </body>
 </html>
