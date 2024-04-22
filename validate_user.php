809aad04278931b7e795a99326dfd18311ac5bf8
Tadhg Ryan <tadhgryan2012@gmail.com>
2024-04-22 01:03:10 +0100

    [Tadhg] -Small fixes for the server -Fixed nav bar for when not logged in.

--- validate_user.php
+++ validate_user.php
@@ -4,6 +4,5 @@ require_once(__DIR__ . '/validator_functions.php');
 try {
     validate_user_logged_in();
 } catch (ValidationException $e) {
-    echo 'User Credentials have expired';
-    exit();
+    header("Location: /login/");
 }
